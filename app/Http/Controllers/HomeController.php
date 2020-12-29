09510746ac3d31a43a181010e5ead40af4aeaf55
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-12-29 18:35:02 +0700

    Controller //

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index($name = null)
+    {
+        return 'Hi from HomeController Name '. $name;
+    }
+}
