6d61ec7e2460512b599c4f9f3fbadbaa6ca30bfa
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-12-30 09:59:18 +0700

    Http Client

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ClientController extends Controller
+{
+    public function getAllPost()
+    {
+        $response = Http::get('https://jsonplaceholder.typicode.com/posts');
+        return $response->json();
+    }
+}
